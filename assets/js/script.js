0e6810b3b89b51227ef7d6f91710d40d2a6a60bd
BILL VELIZ <bvelizmera@gmail.com>
2024-01-31 00:07:42 +0000

    tidying up code for styling and

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 /**
@@ -26,6 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answers has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //Creates two random numbers 1-25
     let num1 = Math.floor(Math.random() * 25) + 1;
